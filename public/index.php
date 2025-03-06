bd703f99ff89166c257efd4ab9b6b0b3cdd0d538
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-03-06 16:24:41 +0100

    changing the code structure + trying to use .php file for .env

--- public/index.php
+++ public/index.php
@@ -1,26 +1,36 @@
 <?php
-// public/index.php
+// Incluir el archivo de configuración
+require_once __DIR__ . '/../src/config/defaults.inc.php';
 
+// Configuración de errores basada en el modo de depuración
+if (DEBUG_MODE === true) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+}
+
+// public/index.php
 require_once __DIR__ . '/../vendor/autoload.php';
+/*
 require_once __DIR__ . '/../src/controllers/DatabaseController.php';
 require_once __DIR__ . '/../src/controllers/AuthController.php';
 require_once __DIR__ . '/../src/controllers/SessionController.php';
 require_once __DIR__ . '/../src/config/i18n.php';
 require_once __DIR__ . '/../src/config/AppInitializer.php';
+*/
+use App\Config\AppInitializer;
 
-use App\AppInitializer;
-
+/*
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
+*/
 
 // Inicializar la aplicación
 list($db, $authController, $session, $twig) = AppInitializer::initialize();
 
 // Cargar el archivo de rutas y pasar las dependencias
 require_once __DIR__ . '/../src/routes.php';
-
-// Agregar variable global para el tema
-/*
-$theme = isset($_COOKIE['theme']) ? $_COOKIE['theme'] : 'light';
-$twig->addGlobal('theme', $theme);
-*/
---
+++ src/config/defaults.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DEFAULT_PAGE_SIZE', 20);
+define('DB_CHARSET', 'utf8');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'fungidb');
+define('DB_USER', 'root');
+define('DB_PASS', 'Root@1234');
+
+define('JWT_SECRET_KEY', 'fungi_secret_key');
+
+// Añadir el modo de depuración por defecto (false para producción)
+define('DEBUG_MODE', true);
