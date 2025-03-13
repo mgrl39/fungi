f8784992234ebf0c9420f66a9e6805c52697c70a
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-03-14 00:17:41 +0100

    removed old javascripts

--- public/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-/* public/assets/js/script.js */
-
-// Archivo JS personalizado para el proyecto Fungi
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('El proyecto Fungi se ha cargado correctamente.');
-});
-
